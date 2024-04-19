8d84f97798d655597a730392a4c1cd7aafcf0f45
NorthCountryEngineer <eric@northcountryengineer.com>
2024-04-19 19:25:39 -0400

    if this works, I'll eat my shoe. Completely overhauling to use modules.

--- ncacademy_website/terraform/variables.tf
+++ ncacademy_website/terraform/variables.tf
@@ -1,12 +1,6 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-variable "ENVIRONMENT" {
-  description = "Deployment environment"
-  type        = string
-  default     = "development"
-}
-
 variable "AWS_REGION" {
   type        = string
   description = "The AWS region to put the bucket into"
@@ -27,59 +21,6 @@ variable "CLOUDFLARE_API_KEY" {
 variable "CLOUDFLARE_EMAIL" {
   type        = string
   description = "Emal used to access Cloudflare"
-}
-
-variable "vpc_parameters" {
-  description = "VPC parameters"
-  type = map(object({
-    cidr_block           = string
-    enable_dns_support   = optional(bool, true)
-    enable_dns_hostnames = optional(bool, true)
-    tags                 = optional(map(string), {})
-  }))
-  default = {}
-}
-
-variable "subnet_parameters" {
-  description = "Subnet parameters"
-  type = map(object({
-    cidr_block = string
-    vpc_name   = string
-    tags       = optional(map(string), {})
-  }))
-  default = {}
-}
-
-variable "igw_parameters" {
-  description = "IGW parameters"
-  type = map(object({
-    vpc_name = string
-    tags     = optional(map(string), {})
-  }))
-  default = {}
-}
-
-variable "rt_parameters" {
-  description = "RT parameters"
-  type = map(object({
-    vpc_name = string
-    tags     = optional(map(string), {})
-    routes = optional(list(object({
-      cidr_block = string
-      use_igw    = optional(bool, true)
-      gateway_id = string
-    })), [])
-  }))
-  default = {}
-}
-
-variable "rt_association_parameters" {
-  description = "RT association parameters"
-  type = map(object({
-    subnet_name = string
-    rt_name     = string
-  }))
-  default = {}
 }
 
 variable "target_availability_zones" {
